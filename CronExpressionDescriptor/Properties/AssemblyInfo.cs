f00cad9e10ae52d346dfefce5922babe20699511
Brady Holt <brady.holt@gmail.com>
2014-03-01 18:44:09 -0600

    updating AssemblyInfo

--- CronExpressionDescriptor/Properties/AssemblyInfo.cs
+++ CronExpressionDescriptor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello")]
+[assembly: AssemblyTitle("Cron Expression Descriptor")]
 [assembly: AssemblyDescription("A library that converts cron expressions into human readable strings.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brady Holt, Renato Lima, Ivan Santos, Siarhei Khalipski")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9")]
-[assembly: AssemblyFileVersion("1.9")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8")]

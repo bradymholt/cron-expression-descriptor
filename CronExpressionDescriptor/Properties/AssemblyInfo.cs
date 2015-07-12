6952c2514fa6ded04a86b119e4df3c7d8bdc5bf0
Brady Holt <b.radyholt@gmail.com>
2015-07-11 22:15:10 -0500

    Release 1.15

--- CronExpressionDescriptor/Properties/AssemblyInfo.cs
+++ CronExpressionDescriptor/Properties/AssemblyInfo.cs
@@ -1,3 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -32,5 +60,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.13.0")]
-[assembly: AssemblyFileVersion("1.13.0")]
+[assembly: AssemblyVersion("1.15.0")]
+[assembly: AssemblyFileVersion("1.15.0")]

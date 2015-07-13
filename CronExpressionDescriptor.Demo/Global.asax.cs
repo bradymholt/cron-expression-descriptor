4d1d5b0f75ba76eb59a92a245a09637da860925c
Brady Holt <b.radyholt@gmail.com>
2015-07-12 22:50:40 -0500

    Make Demo site pretty

--- CronExpressionDescriptor.Demo/Global.asax.cs
+++ CronExpressionDescriptor.Demo/Global.asax.cs
@@ -12,10 +12,7 @@ namespace CronExpressionDescriptorDemo
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }

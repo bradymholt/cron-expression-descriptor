f0e73a56d0e06ea72bd9aba573ee0528b272b3ef
Illegitimis <illegitimis@gmail.com>
2016-05-24 18:38:58 +0300

    Romanian translation
    Decent translation, not 100% accurate, lacking the numeral attribute
    agreement in the genitive case

--- CronExpressionDescriptor/Options.cs
+++ CronExpressionDescriptor/Options.cs
@@ -23,6 +23,7 @@ namespace CronExpressionDescriptor
                 || cultureInfo.Equals(new CultureInfo("de-DE")) //German
                 || cultureInfo.Equals(new CultureInfo("it-IT")) //Italian
                 || cultureInfo.Equals(new CultureInfo("tr-TR")) //Turkish
+                || cultureInfo.Equals(new CultureInfo("ro-RO")) //Romanian                
                 )
             {
                 this.Use24HourTimeFormat = true;

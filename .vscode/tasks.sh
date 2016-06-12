3ac1f69b7ebbfe393436e32c4bb4c162d9487770
Brady Holt <brady.holt@gmail.com>
2016-06-12 15:24:26 -0500

    Setup build/test from vscode

---
+++ .vscode/tasks.sh
@@ -0,0 +1,5 @@
+if [ $1 == "build" ] ; then
+ xbuild /property:GenerateFullPaths=true /p:Configuration=DebugNoDemo
+elif [ $1 == "test" ] ; then
+ mono ./packages/NUnit.ConsoleRunner.3.2.1/tools/nunit3-console.exe CronExpressionDescriptor.Test/CronExpressionDescriptor.Test.csproj
+fi

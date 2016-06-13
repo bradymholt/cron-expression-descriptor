d4a9d189ff43723274ed14d23726f41dbf2c4c1e
Brady Holt <brady.holt@gmail.com>
2016-06-13 17:17:16 -0500

    vscode config for running tests

--- .vscode/tasks.sh
+++ .vscode/tasks.sh
@@ -1,5 +1,5 @@
 if [ $1 == "build" ] ; then
  xbuild /property:GenerateFullPaths=true /p:Configuration=DebugNoDemo
 elif [ $1 == "test" ] ; then
- mono ./packages/NUnit.ConsoleRunner.3.2.1/tools/nunit3-console.exe CronExpressionDescriptor.Test/CronExpressionDescriptor.Test.csproj
+ mono ./packages/NUnit.ConsoleRunner.3.2.1/tools/nunit3-console.exe CronExpressionDescriptor.Test/bin/DebugNoDemo/CronExpressionDescriptor.Test.dll
 fi

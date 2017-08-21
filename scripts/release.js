0208290a0f86090ede8e8499cc36a4779b8ba875
Brady Holt <brady.holt@gmail.com>
2017-08-21 10:33:12 -0500

    Fix jsbash stack ref issue

--- scripts/release.js
+++ scripts/release.js
@@ -11,7 +11,7 @@
 _p = process; args = _p.argv.slice(2); cd = _p.chdir; exit = _p.exit; env = _p.env; echo = console.log;
 _e = "utf-8"; _fs = require("fs"); readFile = (path, encoding = _e) => { return _fs.readFileSync(path, { encoding }) }; writeFile = (path, contents, encoding = _e) => { _fs.writeFileSync(path, contents, { encoding }) };
 $$ = (cmd, stream) => { r = (require("child_process").execSync(cmd, { stdio: stream ? "inherit" : "pipe" }) ); return !!r ? r.toString().replace(/\n$/, "") : null; };
-$ = cmd => { return $(cmd, true); };
+$ = cmd => { return $$(cmd, true); };
 //
 
 // cd to root dir

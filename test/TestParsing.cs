d70ac96ecaafec0fa06a89599573877f01bf84b2
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-03-11 02:01:38 +0300

    Test added

--- test/TestParsing.cs
+++ test/TestParsing.cs
@@ -1,7 +1,21 @@
+using Xunit;
+
 namespace CronExpressionDescriptor.Test
 {
     public class TestParsing
     {
+        [Fact]
+        public void TestWeekDayAndMonthParsing()
+        {
+            var expression = "0 59 23 31 dEc frI *";
+            var options = new Options
+            {
+                Locale = "en"
+            };
 
+            var res = ExpressionDescriptor.GetDescription(expression, options);
+            
+            Assert.Equal("At 11:59 PM, on day 31 of the month, only on Friday, only in December", res);
+        }
     }
 }

f2e4e93c2fbe950a2aa7598a605ad3ce412f29fe
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-03-11 02:10:32 +0300

    Improve test, implement fix

--- test/TestParsing.cs
+++ test/TestParsing.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Threading;
 using Xunit;
 
 namespace CronExpressionDescriptor.Test
@@ -7,15 +9,30 @@ namespace CronExpressionDescriptor.Test
         [Fact]
         public void TestWeekDayAndMonthParsing()
         {
-            var expression = "0 59 23 31 dEc frI *";
+            const string expression = "0 59 23 31 DEC Fri *";
+
             var options = new Options
             {
                 Locale = "en"
             };
 
-            var res = ExpressionDescriptor.GetDescription(expression, options);
-            
-            Assert.Equal("At 11:59 PM, on day 31 of the month, only on Friday, only in December", res);
+            var oldCulture = Thread.CurrentThread.CurrentCulture;
+
+            try
+            {
+                foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                {
+                    Thread.CurrentThread.CurrentCulture = culture;
+
+                    var res = ExpressionDescriptor.GetDescription(expression, options);
+
+                    Assert.Equal("At 11:59 PM, on day 31 of the month, only on Friday, only in December", res);
+                }
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = oldCulture;
+            }
         }
     }
 }

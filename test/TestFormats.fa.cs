9e0f47de1f2a206e5b41713cc75b22d1919f44f1
Brady Holt <brady.holt@gmail.com>
2019-08-22 09:03:12 -0500

    Add Persian tranlation

---
+++ test/TestFormats.fa.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace CronExpressionDescriptor.Test
+{
+  /// <summary>
+  /// Tests for Persian (Farsi) translation
+  /// </summary>
+  public class TestFormatsFA : Support.BaseTestFormats
+  {
+    protected override string GetLocale()
+    {
+      return "fa";
+    }
+
+    [Fact]
+
+    public void TestEveryMinute()
+    {
+      Assert.Equal("هر دقیقه", GetDescription("* * * * *"));
+    }
+  }
+}

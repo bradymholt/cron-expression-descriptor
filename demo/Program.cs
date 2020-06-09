441c613d93915d399b5ed6dd49b6458e6b61da0d
Brady Holt <brady.holt@gmail.com>
2020-06-09 09:24:44 -0500

    Fix path issues on demo site

--- demo/Program.cs
+++ demo/Program.cs
@@ -16,7 +16,7 @@ namespace Demo
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
       builder.RootComponents.Add<App>("app");
 
-      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      //builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
       builder.Services.AddLocalization();
 
       var host = builder.Build();

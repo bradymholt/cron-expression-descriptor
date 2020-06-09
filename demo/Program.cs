0ea0d4809c53147ff93014fd5c9fc42c32f7a4c7
Brady Holt <brady.holt@gmail.com>
2020-06-09 08:36:00 -0500

    Add back support for i18n in Demo app

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,25 +1,42 @@
 using System;
-using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using System.Globalization;
 
-namespace demo
+namespace Demo
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("app");
 
-            await builder.Build().RunAsync();
-        }
+      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddLocalization();
+
+      var host = builder.Build();
+
+      // Set the culture by using the "locale" querystring
+      var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+      var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+      if (result == "")
+      {
+        result = null;
+      }
+
+      if (result != null)
+      {        
+        var culture = new CultureInfo(result);
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+      }
+
+      await host.RunAsync();
     }
+  }
 }
